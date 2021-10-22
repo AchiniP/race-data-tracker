3bb0497c972170104ac204fcf7d7bd2bbb8e67de
achiniP <achi729@gmail.com>
2021-10-22 20:09:59 +0800

    added missing test name

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   testTimeout: 10000,
   collectCoverage: true,
   coverageReporters: ['json', 'html'],
-  coveragePathIgnorePatterns: ['./src/config/*'],
+  coveragePathIgnorePatterns: ['./src/config/*', './src/service/InitService.js'],
   setupFilesAfterEnv: [
     './test/testdb.js',
     './test/jestSetUp.js'
